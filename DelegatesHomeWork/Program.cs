a52229172e30d7046c36bf6ecfdeacde7c9ec7f6
Hadz_xt61@student.itstep.kz <Hadz_xt61@student.itstep.kz>
2021-02-17 18:35:09 +0600

    Добавьте файлы проекта.

---
+++ DelegatesHomeWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using Models;
+using System;
+
+namespace DelegatesHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new ConsoleButton
+            {
+                Boundary = "-",
+                Width = 8,
+                Height = 20,
+                Text = "Button",
+                InsideColor = ConsoleColor.Red,
+                BoundaryColor = ConsoleColor.Yellow,
+                Action = new ActionDelegate(Console.WriteLine)
+            };
+            service.Button();
+            service.Action += new ActionDelegate(Console.WriteLine);
+            service.Action -= new ActionDelegate(Console.WriteLine);
+        }
+    }
+}
---
+++ Services/IActionService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Services
+{
+    public interface IActionService
+    {
+        public void ActionService<T>(params T[] integers);
+    }
+}
